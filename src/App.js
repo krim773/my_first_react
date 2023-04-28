06a17c03dfcd7ae3a072380df3dd730868985566
krim773 <ezbenitez24@gmail.com>
2023-04-28 02:19:38 +0000

    primer react

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import ListaDeTareas from './Components/ListaDeTareas';
+
+function App() {
+  return (
+    <>
+      <h1>Hello Dojo!</h1>
+      <ListaDeTareas />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/ListaDeTareas.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const ListaDeTareas = () => {
+    return (
+        <div>
+            <h2>
+                Things I need to
+            </h2>
+            <ul style={{ listStyleType: "none" }} >
+                <li>* Learn React</li>
+                <li>* Climb Mt. Everst</li>
+                <li>* Run a marathon</li>
+                <li>* Feed the dogs</li>
+            </ul>
+
+        </div>
+    )
+}
+
+export default ListaDeTareas
